1b52dc4fa7718c48e569fbd386cdce52a072793d
jueyang <jueyang22@gmail.com>
2013-08-21 10:28:20 -0400

    jsonp photo call

--- _includes/js/photos.js
+++ _includes/js/photos.js
@@ -1,13 +1,26 @@
 var photoSetId = "{{page.flickr}}";
-var flickrApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json&nojsoncallback=1";
-$.getJSON(flickrApi, function(res) {
+var flickrApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json";
+
+function jsonFlickrApi(res){
 	var photos = res.photoset.photo;
 	for (var i = 0; i<9 ;i++){
 		var photoUrl = "http:\/\/farm"+ photos[i].farm +
 			".staticflickr.com\/" + photos[i].server + 
 			"\/"+ photos[i].id + 
 			"_"+photos[i].secret +
-			"_q.jpg"
-        var pic  = $('#flickr').append('<img class="col4" id="photo'+ i +'" src="'+photoUrl+ '"/>');
+			"_q.jpg";
+		var pic  = $('#flickr').append('<img class="col4" id="photo'+ i +'" src="'+photoUrl+ '"/>');
 	}
+}
+
+$.ajax({
+   type: 'GET',
+    url: flickrApi,
+    async: true,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+       return
+    }
 });
