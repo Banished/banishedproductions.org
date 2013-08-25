44fd7e501a5e550208f7a0a4ef6889a535993309
jueyang <jueyang22@gmail.com>
2013-08-25 12:27:17 -0500

    update photo.js

--- _includes/js/photos.js
+++ _includes/js/photos.js
@@ -1,7 +1,17 @@
 var photoSetId = "{{page.flickr}}";
 var flickrApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json";
 
-function jsonFlickrApi(res){
+$.ajax({
+   type: 'GET',
+    url: flickrApi,
+    async: true,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: jsonFlickrApi
+});
+
+var jsonFlickrApi = function(res){	
 	var photos = res.photoset.photo;
 	for (var i = 0; i<9 ;i++){
 		var photoUrl = "http:\/\/farm"+ photos[i].farm +
@@ -12,15 +22,3 @@ function jsonFlickrApi(res){
 		var pic  = $('#flickr').append('<img class="col4" id="photo'+ i +'" src="'+photoUrl+ '"/>');
 	}
 }
-
-$.ajax({
-   type: 'GET',
-    url: flickrApi,
-    async: true,
-    jsonpCallback: 'jsonCallback',
-    contentType: "application/json",
-    dataType: 'jsonp',
-    success: function(json) {
-       return
-    }
-});
--- _posts/current/2013-08-29-walk-with-me.md
+++ _posts/current/2013-08-29-walk-with-me.md
@@ -5,6 +5,7 @@ image: "https://dl.dropboxusercontent.com/u/17976424/test-dlbox.jpg"
 tags:
 - image
 ---
+<!-- test post-->
 <!-- this post will contain a map, an image, if any -->
 <!-- the content area is for instructions and other info -->
 All you need is an ipod or some such portable music device. Please note: This is not a historical or guided walk.
