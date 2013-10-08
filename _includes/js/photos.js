4e647320c52c46b4f5d6a42ef65b4b5b0b30c052
jueyang <jueyang22@gmail.com>
2013-10-07 22:24:41 -0400

    add link to flickr photos

--- _includes/js/photos.js
+++ _includes/js/photos.js
@@ -1,9 +1,10 @@
 var photoSetId = "{{page.flickr}}";
-var flickrApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json&jsoncallback=showSet";
+	showApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json&jsoncallback=showSet";
+	nsid = "http://www.flickr.com/photos/60776064@N02/";
 
 $.ajax({
    type: 'GET',
-    url: flickrApi,
+    url: showApi,
     async: true,
     jsonpCallback: 'jsonCallback',
     contentType: "application/json",
@@ -14,11 +15,13 @@ $.ajax({
 var showSet = function(res){	
 	var photos = res.photoset.photo;
 	for (var i = 0; i<9 ;i++){
-		var photoUrl = "http:\/\/farm"+ photos[i].farm +
+		var sourceUrl = "http:\/\/farm"+ photos[i].farm +
 			".staticflickr.com\/" + photos[i].server + 
 			"\/"+ photos[i].id + 
 			"_"+photos[i].secret +
 			"_q.jpg";
-		var pic  = $('#flickr').append('<div class="col4 pad1"><img id="photo'+ i +'" src="'+photoUrl+ '"/></div>');
+		var webUrl = nsid + photos[i].id;
+			title = "Click to see the picture on Flickr";
+		pic  = $('#flickr').append('<div class="col4 pad1"><a href="'+ webUrl + '" title="'+ title +'"><img id="photo'+ i +'" src="'+sourceUrl+ '"/></a></div>');
 	}
 }
