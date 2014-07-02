de244b2034d808badd350b808647cd87bac04608
Jue Yang <jueyang22@gmail.com>
2014-07-02 13:06:48 -0400

    Change to flickr request to https in photo.js

--- _includes/js/photos.js
+++ _includes/js/photos.js
@@ -1,5 +1,5 @@
 var photoSetId = "{{page.flickr}}";
-	showApi = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json&jsoncallback=showSet";
+	showApi = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=19493fa9bbf4ab6081b55b2df529144d&photoset_id=" + photoSetId + "&format=json&jsoncallback=showSet";
 	nsid = "http://www.flickr.com/photos/60776064@N02/";
 
 $.ajax({
@@ -15,7 +15,7 @@ $.ajax({
 var showSet = function(res){	
 	var photos = res.photoset.photo;
 	for (var i = 0; i<9 ;i++){
-		var sourceUrl = "http:\/\/farm"+ photos[i].farm +
+		var sourceUrl = "https:\/\/farm"+ photos[i].farm +
 			".staticflickr.com\/" + photos[i].server + 
 			"\/"+ photos[i].id + 
 			"_"+photos[i].secret +
